81b0790263fd2be8119f3165bb6b55c53700ef90
Seve Barrutia <sevebarrutia@me.com>
2021-07-28 00:41:42 -0500

    modify main style

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const helpers = require('./utils/helpers');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/config");
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
